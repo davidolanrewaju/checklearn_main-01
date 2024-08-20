ab1bb8e147a5ae3216b0f231096b538fd8b13e4e
David <davidolanrewaju538@gmail.com>
2024-08-20 06:37:22 +0100

    Implement delete task

---
+++ src/components/DeleteModal.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { defineEmits } from 'vue';
+
+defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(['handleModal']);
+
+const closeModal = () => {
+  emit('handleModal', false);
+};
+
+const deleteTask = () => {
+  emit('confirmDelete');
+};
+</script>
+
+<template>
+  <div
+    v-if="isOpen"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+  >
+    <div class="bg-white rounded-lg shadow-lg w-96 p-6 relative">
+      <div class="flex items-center justify-between mb-4">
+        <h2 class="text-2xl font-lexend font-semibold">Delete Task</h2>
+        <button @click="closeModal" class="text-gray-400 hover:text-gray-600">
+          <i class="pi pi-times text-xl"></i>
+        </button>
+      </div>
+      <p class="font-lexend text-gray-700 mb-6">Are you sure you want to delete this task?</p>
+      <div class="px-10 flex items-center justify-between">
+        <button
+          @click="deleteTask"
+          class="w-28 font-lexend bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition"
+        >
+          Continue
+        </button>
+        <button
+          @click="closeModal"
+          class="w-28 font-lexend bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 transition"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/TaskLists.vue
+++ src/components/TaskLists.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { onMounted, reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
+import DeleteModal from '@/components/DeleteModal.vue';
 
 const state = reactive({
   tasks: [],
   isLoading: true,
+  taskToDelete: null,
 });
 
 onMounted(() => {
@@ -11,13 +13,52 @@ onMounted(() => {
   state.tasks = [...tasks];
   state.isLoading = false;
 });
+
+const isModalOpen = ref(false);
+
+function handleModal(task = null) {
+  state.taskToDelete = task;
+  isModalOpen.value = !isModalOpen.value;
+}
+
+function deleteTask() {
+  if (state.taskToDelete) {
+    state.tasks = state.tasks.filter((task) => task.id !== state.taskToDelete.id);
+    localStorage.setItem('tasks', JSON.stringify(state.tasks));
+    handleModal();
+  }
+}
 </script>
 
 <template>
   <div v-if="state.isLoading">Loading...</div>
   <div v-else>
-    <div v-for="task in state.tasks" :key="task.id">
-      <p>{{ task.name }}</p>
+    <div
+      v-for="task in state.tasks"
+      :key="task.id"
+      class="flex items-center gap-5 my-4 p-4 bg-indigo-300 rounded-md hover:bg-indigo-400 cursor-pointer transition-all duration-500 ease-in-out transform"
+    >
+      <div class="flex items-center gap-5 w-full">
+        <div class="w-8 h-8 border-2 flex items-center justify-center rounded-full">
+          <i class="pi pi-check text-indigo-500 text-sm"></i>
+        </div>
+        <p class="text-white font-lexend text-lg flex-grow">{{ task.name }}</p>
+        <div class="flex items-center gap-3">
+          <div
+            class="w-8 h-8 flex items-center justify-center rounded-full p-5 hover:bg-gray-300 hover:bg-opacity-70 transition-all duration-300 ease-in-out transform"
+          >
+            <i class="pi pi-pen-to-square text-white text-lg"></i>
+          </div>
+          <div
+            @click="handleModal(task)"
+            class="w-8 h-8 flex items-center justify-center rounded-full p-5 hover:bg-gray-300 hover:bg-opacity-70 transition-all duration-300 ease-in-out transform"
+          >
+            <i class="pi pi-times text-white text-lg"></i>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
+
+  <DeleteModal :isOpen="isModalOpen" @confirmDelete="deleteTask" @handleModal="handleModal" />
 </template>
